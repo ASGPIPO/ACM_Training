7cb73c033094e7dde673fcd64fd42c116a346d4a
ASGPIPO <kuandepuqian@foxmail.com>
2025-05-06 12:09:23 +0800

    (Cao)添加循环序列程序解法

--- Cao/UVA-1854-circularSequence.cpp
+++ Cao/UVA-1854-circularSequence.cpp
@@ -1,6 +1,34 @@
+#include <string>
 #include <iostream>
+int isSmaller(std::string &s, int size, int index, int mins) {
+    for(int i = 0; i < size; i++) {
+        if (s[mins + i] < s[index + i]) {
+            return mins;
+        } else if (s[mins + i] > s[index + i]) {
+            return index;
+        }
+        
+    }
+    return mins;
+}
 int main(int argc, char const *argv[])
 {
-    cin >> 
+    int n;
+    std::cin >> n;
+    while(n--) {
+        std::string s;
+        int mins = 0;
+        std::cin >> s;
+        int size = s.size();
+        s +=s;
+        for (int i = 0; i <= size; i++ ){
+            mins = isSmaller(s, size, i, mins);
+        }
+        while(size--) {
+            printf("%c", s[mins++]);
+        }
+        printf("\n");    
+        
+    }
     return 0;
 }
